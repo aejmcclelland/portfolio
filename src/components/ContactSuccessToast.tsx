44e18fe235b3773174e5d743657786508b634a98
aejmcclelland <aejmcclelland@googlemail.com>
2025-04-09 23:16:06 +0100

    corrected commit message

--- src/components/ContactSuccessToast.tsx
+++ src/components/ContactSuccessToast.tsx
@@ -1,31 +1,33 @@
 'use client';
 import { useEffect, useState } from 'react';
 
-export default function ContactSuccessToast() {
-	const [showToast, setShowToast] = useState(false);
+interface ContactSuccessToastProps {
+  status: string;
+}
 
-	const state = { status: 'success' }; // Replace this with the actual state logic
+export default function ContactSuccessToast({ status }: ContactSuccessToastProps) {
+  const [showToast, setShowToast] = useState(false);
 
-	useEffect(() => {
-		if (state.status === 'success') {
-			setShowToast(true);
-			const timer = setTimeout(() => {
-				setShowToast(false);
-			}, 3000);
-			return () => clearTimeout(timer);
-		}
-	}, [state.status]);
+  useEffect(() => {
+    if (status === 'success') {
+      setShowToast(true);
+      const timer = setTimeout(() => {
+        setShowToast(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [status]);
 
-	return (
-		<>
-			{showToast && (
-				<div
-					className='bg-success text-success-content text-center p-6 rounded-lg shadow-md mb-6 transition-all duration-500 ease-in-out transform animate-slide-in'
-					style={{ animation: 'slideOut 0.5s ease-in-out 2.5s forwards' }}>
-					<h3 className='text-xl font-bold mb-2'>Thank you!</h3>
-					<p>Your message has been sent. I&apos;ll be in touch soon.</p>
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      {showToast && (
+        <div
+          className='bg-success text-success-content text-center p-6 rounded-lg shadow-md mb-6 transition-all duration-500 ease-in-out transform animate-slide-in'
+          style={{ animation: 'slideOut 0.5s ease-in-out 2.5s forwards' }}>
+          <h3 className='text-xl font-bold mb-2'>Thank you!</h3>
+          <p>Your message has been sent. I&apos;ll be in touch soon.</p>
+        </div>
+      )}
+    </>
+  );
 }

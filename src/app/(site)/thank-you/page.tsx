1f96d16d7ea94ae409f6854d427a23f8e0d1bcaf
aejmcclelland <aejmcclelland@googlemail.com>
2025-04-10 08:15:06 +0100

    fixed Link to thank you page

--- src/app/(site)/thank-you/page.tsx
+++ src/app/(site)/thank-you/page.tsx
@@ -1,12 +1,14 @@
+
+import Link from 'next/link';
 export default function ThankYou() {
 	return (
 		<div className='min-h-screen flex items-center justify-center px-4 bg-base-100'>
 			<div className='text-center max-w-md'>
 				<h1 className='text-3xl font-bold mb-4'>Thanks for reaching out!</h1>
 				<p className='mb-6'>I&apos;ll get back to you as soon as possible.</p>
-				<a href='/' className='btn btn-primary'>
+				<Link href='/' className='btn btn-primary'>
 					Return Home
-				</a>
+				</Link>
 			</div>
 		</div>
 	);

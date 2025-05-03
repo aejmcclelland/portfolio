fdf317771d3e9be6ad5cac8861a8264b31c959f9
aejmcclelland <aejmcclelland@googlemail.com>
2025-05-03 23:14:50 +0100

    made some style changes to the footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,33 +5,35 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 export default function Footer() {
 	return (
 		<footer className='footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4 mt-auto'>
-			<aside className='grid-flow-col items-center'>
-				<span className='text-lg font-bold tracking-wide -rotate-45 inline-block'>
-					AM
-				</span>
-				<p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-			</aside>
-			<nav className='grid-flow-col gap-4 md:place-self-center md:justify-self-end'>
-				<WobbleIcon>
-					<a
-						href='https://github.com/aejmcclelland'
-						target='_blank'
-						rel='noopener noreferrer'
-						aria-label='GitHub'>
-						<FaGithub className='w-6 h-6 text-neutral-content hover:text-primary' />
-					</a>
-				</WobbleIcon>
+			<div className='w-full flex justify-between items-center flex-wrap sm:flex-nowrap'>
+				<aside className='grid-flow-col items-center'>
+					<span className='text-lg font-bold tracking-wide -rotate-45 inline-block'>
+						AM
+					</span>
+					<p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+				</aside>
+				<nav className='flex gap-6 pr-6 md:place-self-center md:justify-self-end'>
+					<WobbleIcon>
+						<a
+							href='https://github.com/aejmcclelland'
+							target='_blank'
+							rel='noopener noreferrer'
+							aria-label='GitHub'>
+							<FaGithub className='w-6 h-6 text-neutral-content hover:text-primary' />
+						</a>
+					</WobbleIcon>
 
-				<WobbleIcon>
-					<a
-						href='https://linkedin.com/in/aejmcclelland'
-						target='_blank'
-						rel='noopener noreferrer'
-						aria-label='LinkedIn'>
-						<FaLinkedin className='w-6 h-6 text-neutral-content hover:text-primary' />
-					</a>
-				</WobbleIcon>
-			</nav>
+					<WobbleIcon>
+						<a
+							href='https://linkedin.com/in/aejmcclelland'
+							target='_blank'
+							rel='noopener noreferrer'
+							aria-label='LinkedIn'>
+							<FaLinkedin className='w-6 h-6 text-neutral-content hover:text-primary' />
+						</a>
+					</WobbleIcon>
+				</nav>
+			</div>
 		</footer>
 	);
 }

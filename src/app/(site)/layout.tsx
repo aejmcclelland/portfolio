488038f213f400993218a3a8b952dba97a3c5a0b
aejmcclelland <aejmcclelland@googlemail.com>
2025-05-13 08:03:13 +0100

    corrected Script error in layout.tsx

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,6 +4,7 @@ import Footer from '@/components/Footer';
 import '@/styles/globals.css';
 import { Space_Grotesk } from 'next/font/google';
 import ToastProvider from '@/components/ToastProvider';
+import Script from 'next/script';
 
 const grotesk = Space_Grotesk({
 	subsets: ['latin'],
@@ -27,7 +28,10 @@ export default function RootLayout({
 					name='format-detection'
 					content='telephone=no, date=no, email=no, address=no'
 				/>
-				<script src='https://www.google.com/recaptcha/enterprise.js?render=NEXT_PUBLIC_RECAPTCHA_SITE_KEY'></script>
+				<Script
+					src={`https://www.google.com/recaptcha/enterprise.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+					strategy='afterInteractive'
+				/>
 			</head>
 			<body className='font-grotesk flex flex-col min-h-screen'>
 				<Nav />

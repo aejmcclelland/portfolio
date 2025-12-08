8ca591039911e335fc4de5ff23bfe99b3e549886
aejmcclelland <aejmcclelland@googlemail.com>
2025-12-08 19:49:53 +0000

    removed unecessary logo on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,6 @@ export default function Footer() {
 		<footer className='footer sm:footer-horizontal bg-primary text-neutral-content items-center p-4 mt-auto'>
 			<div className='w-full flex justify-between items-center flex-wrap sm:flex-nowrap'>
 				<aside className='grid-flow-col items-center'>
-					<span className='text-lg font-bold tracking-wide -rotate-45 inline-block'>
-						AM
-					</span>
 					<p>Copyright © {new Date().getFullYear()} - All right reserved</p>
 				</aside>
 				<nav className='flex gap-6 pr-6 md:place-self-center md:justify-self-end'>

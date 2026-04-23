555fbc4b3eb440dd9358ec4b1d8368e3239d5712
aejmcclelland <aejmcclelland@googlemail.com>
2026-04-23 22:13:07 +0100

    add: vercel analytics to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import type { ReactNode } from 'react';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function RootLayout({
 	children,
 }: {
 	readonly children: ReactNode;
 }) {
-	return <>{children}</>;
+	return (
+		<>
+			{children}
+			<Analytics />
+		</>
+	);
 }
